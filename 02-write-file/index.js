dd4f4095eb523a62de2a951927c6255cf4a530c7
Tumilo-Denisovich Vladimir <Tumilo.Denisovich@gmail.com>
2023-05-07 23:58:02 +0400

    feat: done 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const output = fs.createWriteStream(
+  path.join(__dirname, './', 'yourself.txt'),
+);
+
+stdout.write('Hello!\nDescribe yourself\n');
+stdin.on('data', data => {
+  const toString = {}.toString;
+  const dataString = data.toString();
+  const exit = 'exit';
+  if (dataString.trim() == exit) {
+    process.exit();
+  } else {
+    stdout.write('Describe yourself more\n');
+
+    output.write(data);
+  }
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => stdout.write('\nGood luck!\n'));
