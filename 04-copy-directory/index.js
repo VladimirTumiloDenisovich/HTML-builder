cd0dbc431223762d51e0a54f9f04bfcdcbf24b7e
Tumilo-Denisovich Vladimir <Tumilo.Denisovich@gmail.com>
2023-05-08 03:42:47 +0400

    fix: do function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,42 +3,46 @@ const fs = require('fs');
 const path = require('path');
 const fsPromises = fs.promises;
 
-fsPromises.mkdir(
-  path.join(__dirname, './', 'files-copy'),
-  { recursive: true });
 
-fs.readdir(
-  path.join(__dirname, './files-copy'),
-  (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        fs.unlink(
-          path.join(__dirname, './files-copy', file),
-          (err) => {
-            if (err) throw err;
-          });
-      });
-      fs.readdir(
-        path.join(__dirname, './files'),
-        (err, files) => {
-          if (err)
-            console.log(err);
-          else {
-            files.forEach(file => {
-              fsPromises.copyFile(
-                path.join(__dirname, './files', file),
-                path.join(__dirname, './files-copy', file))
-                .then(function() {
-                  console.log('File copied - ' + file);
-                })
-                .catch(function(error) {
-                  console.log(error);
-                });
+function copyDir (namePastFolder, pathCopyFolder) {
+  fsPromises.mkdir(
+    path.join(__dirname, './', namePastFolder),
+    { recursive: true });
+
+  fs.readdir(
+    path.join(__dirname, './', namePastFolder),
+    (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          fs.unlink(
+            path.join(__dirname, './' + namePastFolder, file),
+            (err) => {
+              if (err) throw err;
             });
-          }
         });
-    }
-  });
-  
+        fs.readdir(
+          path.join(__dirname, pathCopyFolder),
+          (err, files) => {
+            if (err)
+              console.log(err);
+            else {
+              files.forEach(file => {
+                fsPromises.copyFile(
+                  path.join(__dirname, pathCopyFolder, file),
+                  path.join(__dirname, './' + namePastFolder, file))
+                  .then(function() {
+                    console.log('File copied - ' + file);
+                  })
+                  .catch(function(error) {
+                    console.log(error);
+                  });
+              });
+            }
+          });
+      }
+    });
+}
+
+copyDir('files-copy', './files');
